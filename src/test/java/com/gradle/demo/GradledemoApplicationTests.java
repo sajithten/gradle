8a8b6b4eb49fc8cc9e4dcb72e9c8d71442f0ef6f
Your Name <you@example.com>
2023-01-14 16:04:03 +0530

    first

---
+++ src/test/java/com/gradle/demo/GradledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
